aed8c11992228667740fc8ef1be13c6c5f5d8758
Connor McCracken <connormccracken@hotmail.co.uk>
2026-09-08 07:42:06 +0100

    improved dataset viewing

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from dataset import Dataset
+from pathlib import Path
 import simulate
 import view_dataset
 
-test = Dataset(img_path="~/projects/dissertation/data/ctc/training/BF-C2DL-HSC/01", seg_path="~/projects/dissertation/data/ctc/training/BF-C2DL-HSC/01_ST/SEG", trk_gt_path = "~/projects/dissertation/data/ctc/training/BF-C2DL-HSC/01_GT/TRA")
+dataset_name = "PhC-C2DH-U373"
+root_path = "~/projects/dissertation/data/ctc/training"
+version = "01"
+
+test = Dataset(img_path=Path(root_path, dataset_name, version), 
+               seg_path=Path(root_path, dataset_name, (version + "_ST"), "SEG"), 
+               trk_gt_path = Path(root_path, dataset_name, (version + "_GT"), "TRA"))
+
 test.trk_pred = simulate.run(test)
 
 view_dataset.view(test)
